a4141b13f3b8ba25794a58beb5491cf3b41bed6f
Harsh Trivedi <64466225+harshTrivedi501@users.noreply.github.com>
2020-08-25 23:22:01 +0530

    Final commit 1 (#2)

--- app/src/main/java/learncodeonline/in/mytodolist/MainActivity.java
+++ app/src/main/java/learncodeonline/in/mytodolist/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //added by harsh
+        //added by vaibhav
 
         myDB = new DatabaseHelper(this);
         listView = findViewById(R.id.listview);
